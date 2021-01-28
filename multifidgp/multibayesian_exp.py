352105cb3a87cc345e8c4730747f7ce8a8c8152a
Chien-Yung Tseng <Chienyungtseng@users.noreply.github.com>
2021-01-28 16:31:37 +0800

    Bayesian Exp file update

--- multifidgp/multibayesian_exp.py
+++ multifidgp/multibayesian_exp.py
@@ -35,17 +35,16 @@ class MultiBayesianExp:
     eps = 1.e-10   # Cutoff for comparison to zero
     # model_parameters_L = [sL rL nL]
     # model_parameters_H = [sH rH nH]
-    # hyp = [ sigma_eps_L  sigma_eps_H rho]
-    # hyp = [logsigma1 logtheta1 logsigma2 logtheta2 rho logsigma_eps_L logsigma_eps_H]
     
-    def __init__(self, XData_H, XData_L,
-                 model_parameters_H, model_parameters_L, rho, f):
+    def __init__(self, XData_H, XData_L, KData_H, KData_L,
+                 model_parameters_H, model_parameters_L, rho):
         self.Xdata_H=XData_H
         self.Xdata_L=XData_L
+        self.Kdata_H=KData_H
+        self.Kdata_L=KData_L
         self.model_parameters_H=model_parameters_H
         self.model_parameters_L=model_parameters_L
         self.rho=rho
-        self.f=f
 
     def k(self, X1, X2, model_parameters):
         """Assembles the kriging matrix."""
@@ -59,50 +58,81 @@ class MultiBayesianExp:
         # Assign Exponential variogram model
         K[:n1, :n2] = exponential_variogram_model(model_parameters, d)
         return K
-
-    def utility(self, s):
+    
+    def likelihood(self, d, G):
+        p = np.exp(-0.5*(d-G)**2)
+        return p
+    
+    def MultiKrig(self, s, r_H, r_L):
         X_L = self.Xdata_L
         X_H = self.Xdata_H
-        model_parameters_L = self.model_parameters_L
-        model_parameters_H = self.model_parameters_H
-        
+        y_L = np.log(self.Kdata_L)
+        y_H = np.log(self.Kdata_H)
         rho = self.rho
-        f = self.f
         
-        nH = len(X_H)
-        nL = len(X_L)
-                
-        if f==0:
-            nL=nL+1
-            if s.shape[0]==2:
-                X_L = np.concatenate([X_L, np.array([s])],axis=0)
-            elif s.shape[0]==1:
-                X_L = np.concatenate([X_L, s],axis=0)
-        elif f==1:
-            nH=nH+1
-            if s.shape[0]==2:
-                X_H = np.concatenate([X_H, np.array([s])],axis=0)
-            elif s.shape[0]==1:
-                X_H = np.concatenate([X_H, s],axis=0)
-        X = np.concatenate([X_L, X_H],axis=0)
+        sigma_eps_L = 0
+        sigma_eps_H = 0
 
+        N_L = len(X_L)
+        N_H = len(X_H)
+        N = N_L + N_H
+
+        # K matrix
+        K_LL = self.k(X_L, X_L, np.array([self.model_parameters_L[0], r_L, self.model_parameters_L[2]]))
+        K_LH = rho*self.k(X_L, X_H, np.array([self.model_parameters_L[0], r_L, self.model_parameters_L[2]]))
+        K_HL = rho*self.k(X_H, X_L, np.array([self.model_parameters_L[0], r_L, self.model_parameters_L[2]]))
+        K_HH = (rho**2)*self.k(X_H, X_H, np.array([self.model_parameters_L[0], r_L, self.model_parameters_L[2]])) + self.k(X_H, X_H, np.array([self.model_parameters_H[0], r_H, self.model_parameters_H[2]]))
+        
+        K_LL = K_LL + np.eye(N_L)*sigma_eps_L
+        K_HH = K_HH + np.eye(N_H)*sigma_eps_H
+        
+        K = np.concatenate([np.concatenate([K_LL,K_LH], axis=1),np.concatenate([K_HL,K_HH], axis=1)])      
+        
+        K = K + np.eye(N)*self.eps
+
+        # LU Decomposition
+        P, L, U = sla.lu(K)
+
+        y = np.concatenate([y_L, y_H]) 
+        s = s.reshape(-1)
+        x_star = s.reshape([1,len(s)])
+
+        dim = x_star.shape
+
+        psi1 = rho*self.k(x_star, X_L, np.array([self.model_parameters_L[0], r_L, self.model_parameters_L[2]]))
+        psi2 = rho**2*self.k(x_star, X_H, np.array([self.model_parameters_L[0], r_L, self.model_parameters_L[2]])) + self.k(x_star, X_H, np.array([self.model_parameters_H[0], r_H, self.model_parameters_H[2]]))
+        psi = np.concatenate([psi1, psi2], axis=1)
+        
+        # calculate prediction
+        mean_star = psi@(la.inv(U)@(la.inv(L)@(P.T@y)))
+        var_star = rho**2*self.k(x_star, x_star, np.array([self.model_parameters_L[0], r_L, self.model_parameters_L[2]])) + self.k(x_star, x_star, np.array([self.model_parameters_H[0], r_H, self.model_parameters_H[2]])) - psi@(la.inv(U)@(la.inv(L)@(P.T@psi.T))) 
+        var_star = abs(np.diag(var_star))
+                    
+        mean_lognormal = np.exp(mean_star + 0.5*var_star)
+        var_lognormal = (np.exp(var_star)-1)*np.exp(2*mean_star+var_star)
+                
+        output = np.concatenate([mean_lognormal, var_lognormal])
+        return output
+
+    def utility(self, s):
+        N=100 # Gaussian samplings
+        d=abs(np.random.normal(loc=self.MultiKrig(s, self.model_parameters_H[1], self.model_parameters_L[1])[0], scale=self.MultiKrig(s, self.model_parameters_H[1], self.model_parameters_L[1])[1], size=N))
+        r_H=abs(np.random.normal(loc=self.model_parameters_H[1], scale=0.01*self.model_parameters_H[1], size=N))
+        r_L=abs(np.random.normal(loc=self.model_parameters_L[1], scale=0.01*self.model_parameters_H[1], size=N))
+              
+        rho = self.rho
+        
+        s = s.reshape(-1)
+        s = s.reshape([1,len(s)])
+                                 
         Utility = 0
-        if X.size==len(X):
-            for i in range(nL):
-                prior = 1/nL*np.sum(self.k(np.array([X_L[i]]),X_L,model_parameters_L)) + 1/nH*(rho**2*np.sum(self.k(np.array([X_L[i]]),X_H,model_parameters_L)) + np.sum(self.k(np.array([X_L[i]]),X_H,model_parameters_H)))
-                Utility = Utility + 1/(nL+nH)*(np.log(self.k(np.array([X_L[i]]),np.array([X_L[i]]),model_parameters_L))-np.log(prior))
-            for i in range(nH):
-                prior = 1/nL*np.sum(self.k(np.array([X_H[i]]),X_L,model_parameters_L)) + 1/nH*(rho**2*np.sum(self.k(np.array([X_H[i]]),X_H,model_parameters_L)) + np.sum(self.k(np.array([X_H[i]]),X_H,model_parameters_H)))
-                Utility = Utility + 1/(nL+nH)*(np.log(rho**2*self.k(np.array([X_H[i]]),np.array([X_H[i]]),model_parameters_L)+self.k(np.array([X_H[i]]),np.array([X_H[i]]),model_parameters_H))-np.log(prior))
-        else:
-            for i in range(nL):
-                prior = 1/nL*np.sum(self.k(np.array([X_L[i,:]]),X_L,model_parameters_L)) + 1/nH*(rho**2*np.sum(self.k(np.array([X_L[i,:]]),X_H,model_parameters_L)) + np.sum(self.k(np.array([X_L[i,:]]),X_H,model_parameters_H)))
-                Utility = Utility + 1/(nL+nH)*(np.log(self.k(np.array([X_L[i,:]]),np.array([X_L[i,:]]),model_parameters_L))-np.log(prior))
-            for i in range(nH):
-                prior = 1/nL*np.sum(self.k(np.array([X_H[i,:]]),X_L,model_parameters_L)) + 1/nH*(rho**2*np.sum(self.k(np.array([X_H[i,:]]),X_H,model_parameters_L)) + np.sum(self.k(np.array([X_H[i,:]]),X_H,model_parameters_H)))
-                Utility = Utility + 1/(nL+nH)*(np.log(rho**2*self.k(np.array([X_H[i,:]]),np.array([X_H[i,:]]),model_parameters_L)+self.k(np.array([X_H[i,:]]),np.array([X_H[i,:]]),model_parameters_H))-np.log(prior))
+        for i in range(N):
+            prior = 0
+            for j in range(N):
+                prior = prior + 1/N*(self.likelihood(d[i],self.MultiKrig(s, r_H[j], r_L[j])[0]))
+            Utility = Utility + 1/N*(np.log(self.likelihood(d[i],self.MultiKrig(s, r_H[i], r_L[i])[0]))-np.log(prior))    
         Utility=-Utility
-        #print(Utility,s)
+        print("Utility = ", -Utility)
         return Utility
     
     def gradient(self, s):
@@ -112,105 +142,80 @@ class MultiBayesianExp:
         if len(s)==1:
             grad = (self.utility(s+dx)-self.utility(s-dx))/(2*dx)
         if len(s)==2:
-            #if s.shape[0]==1:
-                #s = np.array([s[0,0],s[0,1]])
             grad = np.zeros(2)
             grad[0] = (self.utility(s+np.array([dx,0]))-self.utility(s-np.array([dx,0])))/(2*dx)
             grad[1] = (self.utility(s+np.array([0,dy]))-self.utility(s-np.array([0,dy])))/(2*dy)
         if len(s)==3:
-            #if s.shape[0]==1:
-                #s = np.array([s[0,0],s[0,1],s[0,2]])
             grad = np.zeros(3)
             grad[0] = (self.utility(s+np.array([dx,0,0]))-self.utility(s-np.array([dx,0,0])))/(2*dx)
             grad[1] = (self.utility(s+np.array([0,dy,0]))-self.utility(s-np.array([0,dy,0])))/(2*dy)
             grad[2] = (self.utility(s+np.array([0,0,dz]))-self.utility(s-np.array([0,0,dz])))/(2*dz)
         return grad
-
-    def utility_new(self, s):
-        X_L = self.Xdata_L
-        X_H = self.Xdata_H
-        model_parameters_L = self.model_parameters_L
-        model_parameters_H = self.model_parameters_H
-        
-        rho = self.rho
-        f = self.f
-        
-        nH = len(X_H)
-        nL = len(X_L)
-        
-        X_L_old = X_L
-        X_H_old = X_H
-        nH_old = len(X_H_old)
-        nL_old = len(X_L_old)
-        
-        if f==0:
-            nL=nL+1
-            if s.shape[0]==2:
-                X_L = np.concatenate([X_L, np.array([s])],axis=0)
-            elif s.shape[0]==1:
-                X_L = np.concatenate([X_L, s],axis=0)
-        elif f==1:
-            nH=nH+1
-            if s.shape[0]==2:
-                X_H = np.concatenate([X_H, np.array([s])],axis=0)
-            elif s.shape[0]==1:
-                X_H = np.concatenate([X_H, s],axis=0)
-        X = np.concatenate([X_L, X_H],axis=0)
-
-        Utility = 0
-        
-        if X.size==len(X):
-            for i in range(nL_old):
-                prior = 1/nL_old*np.sum(self.k(np.array([X_L_old[i]]),X_L_old,model_parameters_L)) + 1/nH_old*(rho**2*np.sum(self.k(np.array([X_L_old[i]]),X_H_old,model_parameters_L)) + np.sum(self.k(np.array([X_L_old[i]]),X_H_old,model_parameters_H)))
-                Utility = Utility + 1/(nL_old+nH_old)*(np.log(self.k(np.array([X_L[i]]),np.array([X_L[i]]),model_parameters_L))-np.log(prior))
-                #Utility = Utility + 1/(nL_old+nH_old)*(np.log(np.sum(self.k(np.array([X_L[i,:]]),X_L,model_parameters_L)))-np.log(prior))
-            for i in range(nH_old):
-                prior = 1/nL_old*np.sum(self.k(np.array([X_H_old[i]]),X_L_old,model_parameters_L)) + 1/nH_old*(rho**2*np.sum(self.k(np.array([X_H_old[i]]),X_H_old,model_parameters_L)) + np.sum(self.k(np.array([X_H_old[i]]),X_H_old,model_parameters_H)))
-                Utility = Utility + 1/(nL_old+nH_old)*(np.log(rho**2*self.k(np.array([X_H[i]]),np.array([X_H[i]]),model_parameters_L)+self.k(np.array([X_H[i]]),np.array([X_H[i]]),model_parameters_H))-np.log(prior))
-                #Utility = Utility + 1/(nL_old+nH_old)*(np.log(np.sum(rho**2*self.k(np.array([X_H[i,:]]),X_H,model_parameters_L)+self.k(np.array([X_H[i,:]]),X_H,model_parameters_H)))-np.log(prior))            
-        else:
-            for i in range(nL_old):
-                prior = 1/nL_old*np.sum(self.k(np.array([X_L_old[i,:]]),X_L_old,model_parameters_L)) + 1/nH_old*(rho**2*np.sum(self.k(np.array([X_L_old[i,:]]),X_H_old,model_parameters_L)) + np.sum(self.k(np.array([X_L_old[i,:]]),X_H_old,model_parameters_H)))
-                Utility = Utility + 1/(nL_old+nH_old)*(np.log(self.k(np.array([X_L[i,:]]),np.array([X_L[i,:]]),model_parameters_L))-np.log(prior))
-                #Utility = Utility + 1/(nL_old+nH_old)*(np.log(np.sum(self.k(np.array([X_L[i,:]]),X_L,model_parameters_L)))-np.log(prior))
-            for i in range(nH_old):
-                prior = 1/nL_old*np.sum(self.k(np.array([X_H_old[i,:]]),X_L_old,model_parameters_L)) + 1/nH_old*(rho**2*np.sum(self.k(np.array([X_H_old[i,:]]),X_H_old,model_parameters_L)) + np.sum(self.k(np.array([X_H_old[i,:]]),X_H_old,model_parameters_H)))
-                Utility = Utility + 1/(nL_old+nH_old)*(np.log(rho**2*self.k(np.array([X_H[i,:]]),np.array([X_H[i,:]]),model_parameters_L)+self.k(np.array([X_H[i,:]]),np.array([X_H[i,:]]),model_parameters_H))-np.log(prior))
-                #Utility = Utility + 1/(nL_old+nH_old)*(np.log(np.sum(rho**2*self.k(np.array([X_H[i,:]]),X_H,model_parameters_L)+self.k(np.array([X_H[i,:]]),X_H,model_parameters_H)))-np.log(prior))
-        Utility=-Utility
-        #print(Utility,s)
-        return Utility
     
-    def gradient_new(self, s):
-        dx = 0.01
-        dy = 0.01
-        dz = 0.01
-        if len(s)==1:
-            grad = (self.utility_new(s+dx)-self.utility_new(s-dx))/(2*dx)
-        if len(s)==2:
-            #if s.shape[0]==1:
-                #s = np.array([s[0,0],s[0,1]])
-            grad = np.zeros(2)
-            grad[0] = (self.utility_new(s+np.array([dx,0]))-self.utility_new(s-np.array([dx,0])))/(2*dx)
-            grad[1] = (self.utility_new(s+np.array([0,dy]))-self.utility_new(s-np.array([0,dy])))/(2*dy)
-        if len(s)==3:
-            #if s.shape[0]==1:
-                #s = np.array([s[0,0],s[0,1],s[0,2]])
-            grad = np.zeros(3)
-            grad[0] = (self.utility_new(s+np.array([dx,0,0]))-self.utility_new(s-np.array([dx,0,0])))/(2*dx)
-            grad[1] = (self.utility_new(s+np.array([0,dy,0]))-self.utility_new(s-np.array([0,dy,0])))/(2*dy)
-            grad[2] = (self.utility_new(s+np.array([0,0,dz]))-self.utility_new(s-np.array([0,0,dz])))/(2*dz)
-        return grad
+    # Find maximum Utility by assigning uniform grid with resolution (res) for the sampling location candidates
+    def execute_max(self, bnd, res):
+        if len(bnd)==1:
+            bndx = bnd
+            linx = np.linspace(bndx[0], bndx[1], int(abs(bndx[0]-bndx[1])/res)+1)
+            s = np.zeros(len(linx))
+            Utility = np.zeros(len(s))
+            for i in range(len(linx)):
+                s[i] = linx[i]
+                Utility[i] = self.utility(s[i])   
+            maxU = np.min(Utility)
+            smax = s[Utility==maxU]
+            smax = smax.reshape(-1)
+            print("max Utility = ", -maxU, " s = ", smax)
+        elif len(bnd)==2:
+            bndx = bnd[0, :]
+            bndy = bnd[1, :]
+            linx = np.linspace(bndx[0], bndx[1], int(abs(bndx[0]-bndx[1])/res)+1)
+            liny = np.linspace(bndy[0], bndy[1], int(abs(bndy[0]-bndy[1])/res)+1)
+            s = np.zeros([len(linx)*len(liny), 2])
+            Utility = np.zeros(len(s))
+            for i in range(len(linx)):
+                for j in range(len(liny)):
+                    s[i*len(liny)+j, 0] = linx[i]
+                    s[i*len(liny)+j, 1] = liny[j]
+                    Utility[i*len(liny)+j] = self.utility(s[i*len(liny)+j, :])   
+            maxU = np.min(Utility)
+            smax = s[Utility==maxU, :]
+            smax = smax.reshape(-1)
+            print("max Utility = ", -maxU, " s = ", smax)
+        elif len(bnd)==3:
+            bndx = bnd[0, :]
+            bndy = bnd[1, :]
+            bndz = bnd[2, :]
+            linx = np.linspace(bndx[0], bndx[1], int(abs(bndx[0]-bndx[1])/res)+1)
+            liny = np.linspace(bndy[0], bndy[1], int(abs(bndy[0]-bndy[1])/res)+1)
+            linz = np.linspace(bndz[0], bndz[1], int(abs(bndz[0]-bndz[1])/res)+1)
+            s = np.zeros([len(linx)*len(liny)*len(linz), 3])
+            Utility = np.zeros(len(s))
+            for i in range(len(linx)):
+                for j in range(len(liny)):
+                    for k in range(len(linz)):
+                        s[i*len(liny)*len(linz)+j*len(linz)+k, 0] = linx[i]
+                        s[i*len(liny)*len(linz)+j*len(linz)+k, 1] = liny[j]
+                        s[i*len(liny)*len(linz)+j*len(linz)+k, 2] = linz[k]
+                        Utility[i*len(liny)*len(linz)+j*len(linz)+k] = self.utility(s[i*len(liny)*len(linz)+j*len(linz)+k, :])   
+            maxU = np.min(Utility)
+            smax = s[Utility==maxU, :]
+            smax = smax.reshape(-1)
+            print("max Utility = ", -maxU, " s = ", smax)
+        else:
+            raise Exception("The input sampling location is not in 1D, 2D, or 3D coordinate!")            
+        return smax
 
-    def execute(self, inis):
+    # Find maximum Utility by performing numerical optimization for the sampling location candidates
+    def execute_optimization(self, inis):
         if len(inis)==1:
-            bnds = ((-40, 10))
+            bnds = ((-30, 5))
         if len(inis)==2:
-            bnds = ((-40, 10), (-15, 30))
+            bnds = ((-30, 5), (-10, 25))
         if len(inis)==3:
-            bnds = ((-40, 10), (-15, 30), (0, 200))
-        Result = op.minimize(fun = self.utility_new, x0 = inis, method = 'Powell', bounds = bnds)
-        #Result = op.minimize(fun = self.utility_new, x0 = inis, method = 'Newton-CG', jac = self.gradient_new, bounds = bnds)
+            bnds = ((-30, 5), (-10, 25), (0, 200))
+        Result = op.minimize(fun = self.utility, x0 = inis, method = 'Powell', bounds = bnds)
+        #Result = op.minimize(fun = self.utility, x0 = inis, method = 'TNC', jac = self.gradient, bounds = bnds)
         print('Powell Optimization details:')
         print(Result)
         s = Result.x
--- multifidgp/singlebayesian_exp.py
+++ multifidgp/singlebayesian_exp.py
@@ -33,13 +33,11 @@ from multifidgp.variogram_models import exponential_variogram_model
 class SingleBayesianExp:
 
     eps = 1.e-10   # Cutoff for comparison to zero
-    # model_parameters_L = [sL rL nL]
-    # model_parameters_H = [sH rH nH]
-    # hyp = [ sigma_eps_L  sigma_eps_H rho]
-    # hyp = [logsigma1 logtheta1 logsigma2 logtheta2 rho logsigma_eps_L logsigma_eps_H]
+    # model_parameters = [s r n]
     
-    def __init__(self, XData, model_parameters):
+    def __init__(self, XData, KData, model_parameters):
         self.Xdata=XData
+        self.Kdata=KData
         self.model_parameters=model_parameters
 
     def k(self, X1, X2, model_parameters):
@@ -54,36 +52,55 @@ class SingleBayesianExp:
         # Assign Exponential variogram model
         K[:n1, :n2] = exponential_variogram_model(model_parameters, d)
         return K
+    
+    def likelihood(self, d, G):
+        p = np.exp(-0.5*(d-G)**2)
+        return p
+    
+    def SingleKrig(self, s, r):
+        X = self.Xdata
+        y = np.log(self.Kdata)
+        
+        N = len(X)
+
+        # K matrix
+        K = self.k(X, X, self.model_parameters) 
+        K = K + np.eye(N)*self.eps
+        
+        # LU Decomposition
+        P, L, U = sla.lu(K)
+
+        s = s.reshape(-1)
+        x_star = s.reshape([1,len(s)])
+        dim = x_star.shape
+
+        psi = self.k(x_star, X, self.model_parameters)
+        
+        # calculate prediction
+        mean_star = psi@(la.inv(U)@(la.inv(L)@(P.T@y)))
+        var_star = self.k(x_star, x_star, self.model_parameters) - psi@(la.inv(U)@(la.inv(L)@(P.T@psi.T))) 
+        var_star = abs(np.diag(var_star))
+
+        mean_lognormal = np.exp(mean_star + 0.5*var_star)
+        var_lognormal = (np.exp(var_star)-1)*np.exp(2*mean_star+var_star)     
+        return mean_lognormal, var_lognormal
 
     def utility(self, s):
-        X = self.Xdata
-        model_parameters = self.model_parameters
+        N=100 # Gaussian samplings
+        d=abs(np.random.normal(loc=self.SingleKrig(s, self.model_parameters[1])[0], scale=self.SingleKrig(s, self.model_parameters_H[1])[1], size=N))
+        r=abs(np.random.normal(loc=self.model_parameters[1], scale=0.2*self.model_parameters[1], size=N))
         
-        n = len(X)
-        
-        X_old = X
-        n_old = len(X_old)
-        
-        n=n+1
-        if s.shape[0]==2:
-            X = np.concatenate([X, np.array([s])],axis=0)
-        elif s.shape[0]==1:
-            X = np.concatenate([X, s],axis=0)
-
+        s = s.reshape(-1)
+        s = s.reshape([1,len(s)])
+                                 
         Utility = 0
-        
-        if X.size==len(X):
-            for i in range(n_old):
-                prior = 1/n_old*np.sum(self.k(np.array([X_old[i]]),X_old,model_parameters))
-                Utility = Utility + 1/n*(np.log(self.k(np.array([X[i]]),np.array([X[i]]),model_parameters))-np.log(prior))
-                #Utility = Utility + 1/n*(np.log(np.sum(self.k(np.array([X[i]]),X,model_parameters)))-np.log(prior))
-        else:
-            for i in range(n_old):
-                prior = 1/n_old*np.sum(self.k(np.array([X_old[i,:]]),X_old,model_parameters))
-                Utility = Utility + 1/n*(np.log(self.k(np.array([X[i,:]]),np.array([X[i,:]]),model_parameters))-np.log(prior))
-                #Utility = Utility + 1/n*(np.log(np.sum(self.k(np.array([X[i,:]]),X,model_parameters)))-np.log(prior))
+        for i in range(N):
+            prior = 0
+            for j in range(N):
+                prior = prior + 1/N*(self.likelihood(d[i],self.SingleKrig(s, r[j])[0]))
+            Utility = Utility + 1/N*(np.log(self.likelihood(d[i],self.SingleKrig(s, r[i])[0]))-np.log(prior))    
         Utility=-Utility
-        #print(Utility,s)
+        print("Utility = ", -Utility)
         return Utility
     
     def gradient(self, s):
@@ -91,31 +108,82 @@ class SingleBayesianExp:
         dy = 0.01
         dz = 0.01
         if len(s)==1:
-            grad = (self.utility_new(s+dx)-self.utility_new(s-dx))/(2*dx)
+            grad = (self.utility(s+dx)-self.utility(s-dx))/(2*dx)
         if len(s)==2:
-            #if s.shape[0]==1:
-                #s = np.array([s[0,0],s[0,1]])
             grad = np.zeros(2)
-            grad[0] = (self.utility_new(s+np.array([dx,0]))-self.utility_new(s-np.array([dx,0])))/(2*dx)
-            grad[1] = (self.utility_new(s+np.array([0,dy]))-self.utility_new(s-np.array([0,dy])))/(2*dy)
+            grad[0] = (self.utility(s+np.array([dx,0]))-self.utility(s-np.array([dx,0])))/(2*dx)
+            grad[1] = (self.utility(s+np.array([0,dy]))-self.utility(s-np.array([0,dy])))/(2*dy)
         if len(s)==3:
-            #if s.shape[0]==1:
-                #s = np.array([s[0,0],s[0,1],s[0,2]])
             grad = np.zeros(3)
-            grad[0] = (self.utility_new(s+np.array([dx,0,0]))-self.utility_new(s-np.array([dx,0,0])))/(2*dx)
-            grad[1] = (self.utility_new(s+np.array([0,dy,0]))-self.utility_new(s-np.array([0,dy,0])))/(2*dy)
-            grad[2] = (self.utility_new(s+np.array([0,0,dz]))-self.utility_new(s-np.array([0,0,dz])))/(2*dz)
+            grad[0] = (self.utility(s+np.array([dx,0,0]))-self.utility(s-np.array([dx,0,0])))/(2*dx)
+            grad[1] = (self.utility(s+np.array([0,dy,0]))-self.utility(s-np.array([0,dy,0])))/(2*dy)
+            grad[2] = (self.utility(s+np.array([0,0,dz]))-self.utility(s-np.array([0,0,dz])))/(2*dz)
         return grad
 
-    def execute(self, inis):
+    # Find maximum Utility by assigning uniform grid with resolution (res) for the sampling location candidates    
+    def execute_max(self, bnd, res):
+        if len(bnd)==1:
+            bndx = bnd
+            linx = np.linspace(bndx[0], bndx[1], int(abs(bndx[0]-bndx[1])/res)+1)
+            s = np.zeros(len(linx))
+            Utility = np.zeros(len(s))
+            for i in range(len(linx)):
+                s[i] = linx[i]
+                Utility[i] = self.utility(s[i])   
+            maxU = np.min(Utility)
+            smax = s[Utility==maxU]
+            max = smax.reshape(-1)
+            print("max Utility = ", -maxU, " s = ", smax)
+        elif len(bnd)==2:
+            bndx = bnd[0, :]
+            bndy = bnd[1, :]
+            linx = np.linspace(bndx[0], bndx[1], int(abs(bndx[0]-bndx[1])/res)+1)
+            liny = np.linspace(bndy[0], bndy[1], int(abs(bndy[0]-bndy[1])/res)+1)
+            s = np.zeros([len(linx)*len(liny), 2])
+            Utility = np.zeros(len(s))
+            for i in range(len(linx)):
+                for j in range(len(liny)):
+                    s[i*len(liny)+j, 0] = linx[i]
+                    s[i*len(liny)+j, 1] = liny[j]
+                    Utility[i*len(liny)+j] = self.utility(s[i*len(liny)+j, :])   
+            maxU = np.min(Utility)
+            smax = s[Utility==maxU, :]
+            max = smax.reshape(-1)
+            print("max Utility = ", -maxU, " s = ", smax)
+        elif len(bnd)==3:
+            bndx = bnd[0, :]
+            bndy = bnd[1, :]
+            bndz = bnd[2, :]
+            linx = np.linspace(bndx[0], bndx[1], int(abs(bndx[0]-bndx[1])/res)+1)
+            liny = np.linspace(bndy[0], bndy[1], int(abs(bndy[0]-bndy[1])/res)+1)
+            linz = np.linspace(bndz[0], bndz[1], int(abs(bndz[0]-bndz[1])/res)+1)
+            s = np.zeros([len(linx)*len(liny)*len(linz), 3])
+            Utility = np.zeros(len(s))
+            for i in range(len(linx)):
+                for j in range(len(liny)):
+                    for k in range(len(linz)):
+                        s[i*len(liny)*len(linz)+j*len(linz)+k, 0] = linx[i]
+                        s[i*len(liny)*len(linz)+j*len(linz)+k, 1] = liny[j]
+                        s[i*len(liny)*len(linz)+j*len(linz)+k, 2] = linz[k]
+                        Utility[i*len(liny)*len(linz)+j*len(linz)+k] = self.utility(s[i*len(liny)*len(linz)+j*len(linz)+k, :])   
+            maxU = np.min(Utility)
+            smax = s[Utility==maxU, :]
+            max = smax.reshape(-1)
+            print("max Utility = ", -maxU, " s = ", smax)
+        else:
+            raise Exception("The input sampling location is not in 1D, 2D, or 3D coordinate!")            
+        return smax
+
+    # Find maximum Utility by performing numerical optimization for the sampling location candidates
+    def execute_optimization(self, inis):
         if len(inis)==1:
-            bnds = ((-40, 10))
+            bnds = ((-30, 5))
         if len(inis)==2:
-            bnds = ((-40, 10), (-15, 30))
+            bnds = ((-30, 5), (-10, 25))
         if len(inis)==3:
-            bnds = ((-40, 10), (-15, 30), (0, 200))
+            bnds = ((-30, 5), (-10, 25), (0, 200))
         Result = op.minimize(fun = self.utility, x0 = inis, method = 'Powell', bounds = bnds)
-        #Result = op.minimize(fun = self.utility, x0 = inis, method = 'Newton-CG', jac = self.gradient, bounds = bnds)
+        #Result = op.minimize(fun = self.utility, x0 = inis, method = 'SLSQP', jac = self.gradient)#, bounds = bnds)
         print('Powell Optimization details:')
         print(Result)
         s = Result.x
